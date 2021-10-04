ebb068f6f31e9cfb7b0fa2c68fa77a46d01f8918
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2021-10-03 21:24:26 -0300

    Agregar archivos de proyecto.

---
+++ ShowInfoWindow.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace VSIXProjectMiqueas
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("0e49907d-c9a4-4d82-93e1-00045e08c6a5")]
+    public class ShowInfoWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowInfoWindow"/> class.
+        /// </summary>
+        public ShowInfoWindow() : base(null)
+        {
+            this.Caption = "ShowInfoWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ShowInfoWindowControl();
+        }
+    }
+}
---
+++ ShowInfoWindowControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.IO;
+using System.Net;
+
+namespace VSIXProjectMiqueas
+{
+    /// <summary>
+    /// Interaction logic for ShowInfoWindowControl.
+    /// </summary>
+    public partial class ShowInfoWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowInfoWindowControl"/> class.
+        /// </summary>
+        public ShowInfoWindowControl()
+        {
+            this.InitializeComponent();
+            string result = GettingData();
+            showResponseText.Text = result;
+        }
+
+
+        public string GettingData()
+        {
+            string sURL;
+            sURL = "http://localhost:8000/api/get-report/?id=66";
+            
+            try
+            {
+                var request = WebRequest.Create(sURL);
+                request.Method = "GET";
+                var webResponse = request.GetResponse();
+                var webStream = webResponse.GetResponseStream();
+
+                var reader = new StreamReader(webStream);
+                var data = reader.ReadToEnd();
+                return data;
+            }
+            catch (Exception) // Invalid JSON
+            {
+                return "Error in getting the data";
+            }
+        }
+    }
+}
